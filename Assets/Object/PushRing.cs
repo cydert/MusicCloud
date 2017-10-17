9324c2f19573c681c60c6fe5eacb1e00d8a96ec9
cydert <saida_service@outlook.jp>
2017-10-17 23:58:39 +0900

    音楽再生用クラス準備

---
+++ Assets/Object/PushRing.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PushRing : MonoBehaviour {
+
+    string[] rhythmText;  //タイミング, 座標
+    public GameObject ring; //インスタンス化
+    public GameObject ringPosition;
+    private int cnt = 0;    //現在出力したリング数
+    private int nextTiming = -1;
+    int barSpace = 8; //何bar前からリングを出す
+
+	// Use this for initialization
+	void Start () {
+        //Debug
+        startMusic(MUSIC.Melt);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(nextTiming != -1 && rhythmText != null)
+        {
+            if(nextTiming - Music.MusicalTimeBar <= barSpace)
+            {
+                Debug.Log("put:"+Music.MusicalTimeBar);
+                put();
+            }
+        }
+
+
+    }
+
+    private void put()
+    {
+        string[] infoAr = rhythmText[cnt].Split(',');   //分割
+        GameObject newRing = Instantiate(ring, ringPosition.transform.position, Quaternion.identity);   //インスタンス化
+        Ring ringCom = newRing.GetComponent<Ring>();
+        ringCom.Init(int.Parse(infoAr[0]), int.Parse(infoAr[1]), int.Parse(infoAr[2])); //座標セット
+        cnt++;
+        if (rhythmText.Length > cnt)    //次のリング
+        {
+            Debug.Log("nx");
+            nextTiming = int.Parse(rhythmText[cnt].Split(',')[0]);
+        }
+        else
+            nextTiming = -1;
+    }
+
+    enum MUSIC
+    {
+        Melt, FlightSound
+    }
+
+    //指定した音楽でスタート
+    void startMusic(MUSIC musicKind)
+    {
+        switch (musicKind)
+        {
+            case MUSIC.Melt:
+                Music.Play("Melt");
+                rhythmText = new string[] { "5,0,0", "20,1,0", "50,5,1" };
+                nextTiming = int.Parse(rhythmText[cnt].Split(',')[0]);
+                //TODO readFile
+                break;
+            case MUSIC.FlightSound:
+                Music.Play("FlightSound");
+                rhythmText = new string[] { "5,0,0", "20,1,0", "50,1,1" };
+                nextTiming = int.Parse(rhythmText[cnt].Split(',')[0]);
+                break;
+        }
+
+    }
+}
--- Assets/Object/Ring.cs
+++ Assets/Object/Ring.cs
@@ -16,11 +16,12 @@ public class Ring : MonoBehaviour {
 
     // Use this for initialization
     void Start () {
+        airCraft = GameObject.Find("AircraftJet");
         planeInfo = airCraft.GetComponent<AeroplaneController>();
         rigid = GetComponent<Rigidbody>();
 
-        Init(8); //Debug用
-        Music.Play("FlightSound");
+        //Init(8, 3, 0); //Debug用
+        //Music.Play("Melt");
 
 
 	}
@@ -32,29 +33,47 @@ public class Ring : MonoBehaviour {
 
     void FixedUpdate()
     {
-        
-        float length = this.transform.position.z - airCraft.transform.position.z; //2点のz距離
-        float toTime = goalTime - Music.MusicalTime;    //指定時間までの時間
+        if (init)
+        {
+            float length = this.transform.position.z - airCraft.transform.position.z; //2点のz距離
+            float toTime = goalTime - Music.MusicalTime;    //指定時間までの時間
 
-        if (length < 0)
-            Destroy(this.gameObject);
-        float ringSpeed = length / toTime;// - planeInfo.ForwardSpeed; //リングのスピードを計算
+            if (length < 0)
+                Destroy(this.gameObject);
 
-        if(debugPrint)
-            Debug.Log("L:"+length +" T:" + toTime+" FS:"+planeInfo.ForwardSpeed + " RS:"+ ringSpeed);
+            float ringSpeed = length / toTime;// - planeInfo.ForwardSpeed; //リングのスピードを計算
+            if (length < 20) ringSpeed = 0;
+
+            if (debugPrint)
+                Debug.Log("L:" + length + " T:" + toTime + " FS:" + planeInfo.ForwardSpeed + " RS:" + ringSpeed);
 
 
-        rigid.velocity = new Vector3(0, 0, -ringSpeed);  //速度設定
-        //rigid.AddForce(new Vector3(0, 0, ringSpeed), ForceMode.Force);
+            rigid.velocity = new Vector3(0, 0, -ringSpeed);  //速度設定
+             //rigid.AddForce(new Vector3(0, 0, ringSpeed), ForceMode.Force);
+        }
         
     }
 
-    //目的のBarを指定すると時間に変更, 動作開始１
-    void Init(int goalBar)
+    // num: 0 ~ 30
+    public void Init(int goalBar, int numX, int numY)
+    {
+        if (numX < 0) numX = 0;
+        else if (numX > 30) numX = 30;
+        if (numY < 0) numY = 0;
+        else if (numY > 30) numY = 30;
+        numX *= 10;
+        numX -= 150;
+        numY *= 10;
+        numY -= 12;
+        Init(goalBar, (float)numX, (float)numY);
+    }
+    //目的のBarを指定すると時間に変更, 動作開始
+    void Init(int goalBar, float x, float y)
     {
         init = true;
         goalTime = goalBar * Music.UnitPerBar;
-
-        
+        transform.position = new Vector3(x, y, transform.position.z);
+        //x: -150 .. 150
+        //y: -12 .. 238
     }
 }
