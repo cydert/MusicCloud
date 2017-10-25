2cafc1664ea2e65a4db0c387508b854e4173a585
cydert <saida_service@outlook.jp>
2017-10-25 21:00:19 +0900

    add: fire

---
+++ Assets/Object/bulletSc.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bulletSc : MonoBehaviour {
+    private float interval = 2;   //指定時間
+    private float time = 0;   //何秒立ったか
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        time += Time.deltaTime; //タイムを加算
+        if (time >= interval)
+        {
+            Destroy(gameObject);  //指定時間になったので削除
+        }
+    }
+}
---
+++ Assets/Plane/FireSc.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireSc : MonoBehaviour {
+    public GameObject bulletIns;    //銃弾のインスタンス
+    public GameObject bulletPosR;   //発射場所
+    public GameObject bulletPosL;
+
+    private int speed = 8;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButton("Fire1"))
+        {
+            Quaternion rote = Quaternion.Euler(90.0f, 0.0f, 0.0f);
+            GameObject bullet = Instantiate(bulletIns, bulletPosR.transform.position, rote);   //インスタンス化
+            Rigidbody buRigid = bullet.GetComponent<Rigidbody>();    //発射
+            buRigid.AddForce(transform.forward*speed, ForceMode.Impulse);
+        }
+
+        if (Input.GetButton("Fire2"))
+        {
+            Quaternion rote = Quaternion.Euler(90.0f, 0.0f, 0.0f);
+            GameObject bullet = Instantiate(bulletIns, bulletPosL.transform.position, rote);   //インスタンス化
+            Rigidbody buRigid = bullet.GetComponent<Rigidbody>();    //発射
+            buRigid.AddForce(transform.forward * speed, ForceMode.Impulse);
+        }
+    }
+}
