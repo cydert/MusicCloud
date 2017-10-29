79f3f345e35df322cb9508996d2491ef82839426
cydert <saida_service@outlook.jp>
2017-10-29 19:03:40 +0900

    add: タイマークラス

---
+++ Assets/Plane/EnemySc.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySc : MonoBehaviour {
+    private int ONE_BULLET = 5;
+    private int nowBullet = 0;
+    private int reloadTime = 3;
+    private Timer timer;
+
+	// Use this for initialization
+	void Start () {
+        timer = new Timer();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(nowBullet == 0)
+        {
+            if (timer.IsStart())    //タイマー動作中か
+            {
+                if (timer.IsEnd()) nowBullet = ONE_BULLET;  //補給
+            }
+            else
+            {
+                timer.Start(reloadTime);    //タイマースタート
+            }
+        }else
+        {
+            //銃弾発射
+
+        }
+	}
+
+    void setOneMaxBullet(int cnt)
+    {
+        ONE_BULLET = cnt;
+    }
+}
---
+++ Assets/Timer.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timer : MonoBehaviour {
+    bool isStart = false;   //動作中か
+    bool isEnd = true;      //終了してるか
+    float setSec;           //指定時間
+    private float nowTime;
+
+    public Timer() { }
+
+    public Timer(float sec)
+    {
+        Start(sec);
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        nowTime += Time.deltaTime; //タイムを加算
+        if (nowTime >= setSec)
+        {
+            nowTime = 0;
+            //指定時間
+            final();    //後処理
+        }
+    }
+
+    public bool Start(float sec)
+    {
+        if (isStart) return false;
+        setSec = sec;
+        init(); //初期化
+        return true;
+    }
+
+    //指定時間になったか
+    public bool IsEnd()
+    {
+        return isEnd;
+    }
+
+    //指定時間になったか
+    public bool IsStart()
+    {
+        return isStart;
+    }
+
+    private void init()
+    {
+        nowTime = 0;
+        isEnd = false;
+        isStart = true;
+    }
+
+    private void final()
+    {
+        nowTime = 0;
+        isEnd = true;
+        isStart = false;
+    }
+}
