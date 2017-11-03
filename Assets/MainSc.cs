b6c8d3e79a2bdd066b6978694db143d64a569a68
cydert <saida_service@outlook.jp>
2017-11-04 00:47:49 +0900

    add: シーン移動

--- Assets/MainSc.cs
+++ Assets/MainSc.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityStandardAssets.CrossPlatformInput;
 
 public class MainSc : MonoBehaviour {
@@ -9,8 +10,13 @@ public class MainSc : MonoBehaviour {
 
     private string[] musicList = { "Flight Sound", "メルト" };
     private int selectNum = 0;
+    private int nowLevel = 1;
+    private int maxLevel = 3;
 
     bool isInput = false;
+    bool isSelectMusic = true;
+    bool isSelectDif = false;
+    bool isLoading = false;
     // Use this for initialization
     void Start () {
 		
@@ -33,19 +39,47 @@ public class MainSc : MonoBehaviour {
             if (roll > 0.9) //右
             {
                 isInput = true;
-                selectNum = selectNumNext(selectNum + 1);
+                if (isSelectMusic)
+                    selectNum = selectNumNext(selectNum + 1);
+                else if (isSelectDif)
+                    nowLevel = fixNum(nowLevel+1, maxLevel);
             }
             else if(roll < -0.9)
             {
                 isInput = true;
-                selectNum = selectNumNext(selectNum - 1);
+                if(isSelectMusic)
+                    selectNum = selectNumNext(selectNum - 1);
+                else if (isSelectDif)
+                    nowLevel = fixNum(nowLevel-1, maxLevel);
             }
+        }
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            if (isSelectMusic)  //音楽決定
+            {
+                isSelectMusic = false;
+                isSelectDif = true;
+            }else if (isSelectDif)
+            {
+                isSelectDif = false;
+                isLoading = true;
+                SceneManager.LoadSceneAsync("GameScene");
+
+            }
+
         }
     }
 
     private int selectNumNext(int num)
     {
         return Mathf.Abs(num % musicList.Length);
+    }
+
+    private int fixNum (int num, int max){
+        int ans = Mathf.Abs(num % max);
+        if (ans == 0) return max;
+        return ans;
     }
 
     void OnGUI()
@@ -55,7 +89,11 @@ public class MainSc : MonoBehaviour {
         int width = int.Parse(screenres[0]);
         int height = int.Parse(screenres[1]);
         GUI.Label(new Rect(width / 2 - 80, height / 3-50 , 80, 120), "Sound Cloud", infoStyle);
-        GUI.Label(new Rect(width / 2 - 80, height / 2+100 , 80, 120), "曲選択\n<ー  "+musicList[selectNum] +"  ー>", infoStyle);
-
+        if(isSelectMusic)
+            GUI.Label(new Rect(width / 2 - 80, height / 2+100 , 80, 120), "曲選択\n<ー  "+musicList[selectNum] +"  ー>", infoStyle);
+        if (isSelectDif)
+            GUI.Label(new Rect(width / 2 - 80, height / 2 + 100, 80, 120), "難易度選択\n<ー  " + nowLevel + "  ー>", infoStyle);
+        if(isLoading)
+            GUI.Label(new Rect(width / 2 - 80, height / 2 + 100, 80, 120), "読み込み中...", infoStyle);
     }
 }
